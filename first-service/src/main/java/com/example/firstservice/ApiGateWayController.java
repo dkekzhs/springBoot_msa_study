ab81319162497718ca084c1b2280f77a9f1b8650
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-12-07 01:37:07 +0900

    feat : add load balancer check

--- first-service/src/main/java/com/example/firstservice/ApiGateWayController.java
+++ first-service/src/main/java/com/example/firstservice/ApiGateWayController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class ApiGateWayController {
+
+    Environment env;
+    @Autowired
+    public ApiGateWayController(Environment env){
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -23,7 +34,9 @@ public class ApiGateWayController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi , there. This is a message from first_service";
+    public String check(HttpServletRequest request){
+        log.info("request Port : {}",request.getServerPort());
+        return String.format("Hi , there. This is a message from first_service %s",
+                env.getProperty("local.server.port"));
     }
 }
