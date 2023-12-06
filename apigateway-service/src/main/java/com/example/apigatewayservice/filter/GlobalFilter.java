816e4269a0b656d1603b3ddd307d37a96c0f404c
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-12-07 00:53:05 +0900

    feat : add GlobalFilter api-gateway

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,45 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("GlobalFilter Pre filter : baseMessage -> {}", config.getBaseMessage());
+            if(config.isPreLogger()){
+                log.info("GlobalFilter pre filter : request id -> {}",request.getId());
+            }
+            //custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->{
+                if(config.isPostLogger()){
+                    log.info("GlobalFilter Post filter : response status -> {}",response.getStatusCode());
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config{
+        //Put Configuration properties
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+    public GlobalFilter(){
+        super(Config.class);
+    }
+
+}
